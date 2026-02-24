5c33c6e2ca59db7035bcc137c9fbea96e207417b
Hamza Rehman <mohdhamzarehman@gmail.com>
2026-02-24 23:44:46 +0530

    changes

--- structs/structs.go
+++ structs/structs.go
@@ -3,15 +3,26 @@ package main
 import "fmt"
 
 func main() {
-	type Person struct {
-		fName string
-		lName string
-		age   int
-	}
 	p := Person{
 		fName: "Hamza",
 		lName: "Rehman",
 		age:   15,
 	}
 	fmt.Println(p.fName)
+	//anonymous structs
+	{
+		username: "user123",
+		email:    "abc@123",
+	}
+	fmt.Println(user.email)
 }
+
+type Person struct {
+	fName string
+	lName string
+	age   int
+}
+user := struct {
+		username string
+		email    string
+	}
