79704d82fd8a12a8f2a143e30a2b09149d161964
Hamza Rehman <mohdhamzarehman@gmail.com>
2026-02-26 23:58:39 +0530

    interfaces in go

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,8 +1,31 @@
 package main
 
+import "math"
+
+type geometry interface {
+	area() float64
+	perim() float64
+}
+type rect struct {
+	width, height float64
+}
+type circle struct {
+	radius float64
+}
+
+func (r rect) area() float64 {
+	return r.width * r.height
+}
+func (r rect) perim() float64 {
+	return 2 * (r.height + r.width)
+}
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (c circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
+
 func main() {
-	type geometry interface {
-		area() float64
-		perim() float64
-	}
+
 }
---
+++ strings_runes/strings_runes.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	message := "Hello Go"
+	message3 := "Hello \nGo"
+	message2 := `Hello \nGo`
+	fmt.Println(message)
+	fmt.Println(message2)
+	fmt.Println(message3)
+}
